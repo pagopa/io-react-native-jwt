34884749adc0c89dde2e9263ee45c8970c020b3a
grausof <grausof@gmail.com>
2023-07-14 15:32:07 +0200

    Add JWT produce and unsecured

---
+++ src/__tests__/unsecured.test.ts
@@ -0,0 +1,34 @@
+import { JWTInvalid } from '../utils/errors';
+import type { JWTPayload } from '../types';
+import { UnsecuredJWT } from '../unsecured';
+
+const jwtPayload: JWTPayload = {
+  iss: 'demo',
+  sub: 'demo',
+  iat: 1675206000,
+  exp: 1706742000,
+};
+
+const encodedUnsecureJwt =
+  'eyJhbGciOiJub25lIn0=.eyJpc3MiOiJkZW1vIiwic3ViIjoiZGVtbyIsImlhdCI6MTY3NTIwNjAwMCwiZXhwIjoxNzA2NzQyMDAwfQ==.';
+
+describe('Unsecured JWT', function () {
+  it('must be encoded correctly', async () => {
+    let unsecured = new UnsecuredJWT(jwtPayload).encode();
+    expect(unsecured).toBe(encodedUnsecureJwt);
+  });
+  it('must fail the encode', async () => {
+    expect(() =>
+      new UnsecuredJWT('' as unknown as JWTPayload).encode()
+    ).toThrowError(TypeError);
+  });
+  it('must be decoded correctly', async () => {
+    let unsecured = UnsecuredJWT.decode(encodedUnsecureJwt, {
+      currentDate: new Date(2023, 7, 14, 0, 0),
+    });
+    expect(unsecured.payload).toMatchObject(jwtPayload);
+  });
+  it('must fail the decode', async () => {
+    expect(() => UnsecuredJWT.decode('')).toThrowError(JWTInvalid);
+  });
+});
---
+++ src/unsecured.ts
@@ -0,0 +1,97 @@
+const { atob, btoa } = require('abab');
+
+import type {
+  JWSHeaderParameters,
+  JWTClaimVerificationOptions,
+  JWTPayload,
+} from './types';
+
+import { JWTInvalid } from './utils/errors';
+import jwtPayload from './jwt_claims_set';
+import { ProduceJWT } from './produce';
+
+export interface UnsecuredResult {
+  payload: JWTPayload;
+  header: JWSHeaderParameters;
+}
+
+export const encoder = new TextEncoder();
+export const decoder = new TextDecoder();
+
+/**
+ * The UnsecuredJWT class is a utility for dealing with `{ "alg": "none" }` Unsecured JWTs.
+ *
+ * @example Encoding
+ *
+ * ```js
+ * const unsecuredJwt = new jose.UnsecuredJWT({ 'urn:example:claim': true })
+ *   .setIssuedAt()
+ *   .setIssuer('urn:example:issuer')
+ *   .setAudience('urn:example:audience')
+ *   .setExpirationTime('2h')
+ *   .encode()
+ *
+ * console.log(unsecuredJwt)
+ * ```
+ *
+ * @example Decoding
+ *
+ * ```js
+ * const payload = jose.UnsecuredJWT.decode(jwt, {
+ *   issuer: 'urn:example:issuer',
+ *   audience: 'urn:example:audience',
+ * })
+ *
+ * console.log(payload)
+ * ```
+ */
+export class UnsecuredJWT extends ProduceJWT {
+  /** Encodes the Unsecured JWT. */
+  encode(): string {
+    const header = btoa(JSON.stringify({ alg: 'none' }));
+    const payload = btoa(JSON.stringify(this._payload));
+
+    return `${header}.${payload}.`;
+  }
+
+  /**
+   * Decodes an unsecured JWT.
+   *
+   * @param jwt Unsecured JWT to decode the payload of.
+   * @param options JWT Claims Set validation options.
+   */
+  static decode(
+    jwt: string,
+    options?: JWTClaimVerificationOptions
+  ): UnsecuredResult {
+    if (typeof jwt !== 'string') {
+      throw new JWTInvalid('Unsecured JWT must be a string');
+    }
+    const {
+      0: encodedHeader,
+      1: encodedPayload,
+      2: signature,
+      length,
+    } = jwt.split('.');
+
+    if (length !== 3 || signature !== '') {
+      throw new JWTInvalid('Invalid Unsecured JWT');
+    }
+
+    let header: JWSHeaderParameters;
+    try {
+      header = JSON.parse(atob(encodedHeader));
+      if (header.alg !== 'none') throw new Error();
+    } catch {
+      throw new JWTInvalid('Invalid Unsecured JWT');
+    }
+
+    const payload = jwtPayload(
+      header,
+      JSON.parse(atob(encodedPayload)),
+      options
+    );
+
+    return { payload, header };
+  }
+}
