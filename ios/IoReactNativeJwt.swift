083f003946b3cbbcb77fb2138f5de61b834f41b7
Francesco Grauso <grausof@gmail.com>
2023-07-18 16:28:39 +0200

    Update ios/IoReactNativeJwt.swift
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

--- ios/IoReactNativeJwt.swift
+++ ios/IoReactNativeJwt.swift
@@ -130,24 +130,18 @@ class IoReactNativeJwt: NSObject {
 
     @objc
     func sha256(_ toHash: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
-        
-         let inputData = toHash.data(using: .utf8)
+        let inputData = toHash.data(using: .utf8)
         if (inputData != nil){
-            
-            
             var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-            
             _ = inputData!.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
                 if let pointer = bytes.baseAddress?.assumingMemoryBound(to: UInt8.self) {
                     CC_SHA256(pointer, CC_LONG(inputData!.count), &digest)
                 }
             }
-            
             var sha256String = ""
             for byte in digest {
                 sha256String += String(format: "%02x", byte)
             }
-            
             resolve(String(
                 decoding: Data(digest).base64EncodedData(),
                 as: UTF8.self
@@ -156,8 +150,6 @@ class IoReactNativeJwt: NSObject {
         } else {
             reject("Error", "Unable to parse string to hash", nil);
         }
-        
-       
     }
 
 
