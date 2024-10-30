4a3919cc56929b2959e0119de407236f52f0a416
Cristiano Tofani <cri.tofani@gmail.com>
2024-10-30 16:00:01 +0100

    make task parametric

--- scripts/nodeEvaluate.js
+++ scripts/nodeEvaluate.js
@@ -1,7 +1,8 @@
 const turboDryJson = process.argv[2];
+const task = process.argv[3];
 
 const cacheStatus = JSON.parse(turboDryJson.trim()).tasks.find(
-  (t) => t.task === 'build:android'
+  (t) => t.task === task
 ).cache.status;
 
 console.log(cacheStatus);
